b5f3ae27363008cbad5763eb550e6d579e4067c4
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-11-03 08:11:17 +0530

    App config file

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,3 +1,11 @@
-const appConfig = () =>{
-    
-}
+import { getEnv } from "../utils/get-env"
+
+const appConfig = () =>({
+    NODE_ENV: getEnv("NODE_ENV", "development"),
+    PORT: getEnv("PORT", "5000"),
+    BASE_PATH: getEnv("BASE_PATH", "/api"),
+    MONGO_URI: getEnv("MONGO_URI", ""),
+
+    SESSION_SECRET: getEnv("SESSION_SECRET"),
+    SESSION_EXPIRES_IN: getEnv("SESSION_EXPIRES_IN"),
+});
