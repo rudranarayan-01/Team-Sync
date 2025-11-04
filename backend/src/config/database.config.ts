c44a4d11bd4fb088b845e42cd8d0bf99f69d2fc0
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-11-04 19:05:38 +0530

    DB config updated

---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { config } from "./app.config";
+
+const connectDatabse = async () => {
+    try {
+        await mongoose.connect(config.MONGO_URI);
+        console.log("Connected to MongoDB");
+    }catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
