53c92dfc9d8c4cadfa1ad76898370245ff792cee
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-11-04 19:18:58 +0530

    Error handler

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,10 +1,20 @@
 
 import { ErrorRequestHandler } from "express";
 
-export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
-    console.error(error.stack);
+export const errorHandler: ErrorRequestHandler = (error, req, res, next):any => {
+
+    console.error(`Error occurred in Path: ${req.path} - `, error);
+
+    if(error instanceof SyntaxError){
+        return res.status(400).json({
+            message:"Invalid JSON payload please check your request body",
+            error: "Invalid JSON payload,BAD_REQUEST"
+        });
+    }
+
+
     res.status(500).json({
         message:"Internal Server Error",
-        error: "Unknown error occured"
+        error: error?.message || "Unknown error occured"
     });
 }
