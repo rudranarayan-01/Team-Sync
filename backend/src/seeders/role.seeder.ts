300e53494a4d249bbe4b8954e143c9ff705d3c11
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-11-08 18:36:07 +0530

    Condition fullfilled

--- backend/src/seeders/role.seeder.ts
+++ backend/src/seeders/role.seeder.ts
@@ -7,7 +7,7 @@ import { RolePermissions } from "../utils/role-permission";
 const seedRoles = async () => {
     console.log("Seeding roles started...");
 
-    try{
+    try {
         await connectDatabse();
         const session = await mongoose.startSession();
         session.startTransaction();
@@ -15,15 +15,24 @@ const seedRoles = async () => {
         console.log("Clearing existing roles...");
         await RoleModel.deleteMany({}).session(session);
 
-        for (const roleName in RolePermissions){
+        for (const roleName in RolePermissions) {
             const role = roleName as keyof typeof RolePermissions;
             const permissions = RolePermissions[role];
 
             // check if the role already exists
             const existingRole = await RoleModel.findOne({ name: role }).session(session);
+            if (!existingRole) {
+                console.log("Creating new role...");
+                const newRole = new RoleModel({
+                    name: role,
+                    permissions: permissions,
+                });
+                await newRole.save({ session });
+                console.log(`Role '${role}' created with permissions:`, permissions);
+            }
         };
-    }catch (error) {
+    } catch (error) {
         console.error("Error seeding roles:", error);
     }
-    
+
 }
