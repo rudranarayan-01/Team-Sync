c4b7d50253a233b0d1b1d8ba1f3c8cf0b5f421b7
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-11-13 21:25:07 +0530

    Route added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,8 @@ import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import workspaceRoutes from "./routes/workspace.route";
 import memberRoutes from "./routes/member.route";
+import projectRoutes from "./routes/project.route";
+import taskRoutes from "./routes/task.route";
 
 
 const app = express();
@@ -58,6 +60,8 @@ app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated ,userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated ,workspaceRoutes);
 app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
+app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
+app.use(`${BASE_PATH}/task`, isAuthenticated, taskRoutes);
 
 
 
